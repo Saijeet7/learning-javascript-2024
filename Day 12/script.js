42a38d7ccf3185c59279f009c708ba1c71abc216
Saijeet Upadhyay <saijeet2019@gmail.com>
2024-02-04 22:13:53 +0545

    Complete : day 15

--- Day 12/script.js
+++ Day 12/script.js
@@ -39,3 +39,24 @@ console.log(companies);
 companies.splice(2, 1, "Ola");
 console.log(companies);
 companies.push("Amazon");
+
+/*Arrays in JS
+
+Arrays in JS : collections of items.
+Generally to store related information same type of information, arrays is used for it(linear). Position/Index matters in array.
+Property that returns a value and method does some type of work.
+Array is not a data type itself but instead it is a special  kind of  object, here for key there is index. Value are stored in a linear fashion.
+We can change the value of array with it index. This cannot be done in arrays. Strings are immutable but array are mutable.
+
+Iterable : where we can iterate and use loop. Array, strings, objects. For of loop is usually preferred  for array loop
+
+Arrays Methods
+
+Push() : add to end
+Pop():delete from end & return
+toString: converts array to string
+Concat(): joins multiple arrays & returns result
+Unshift: add to start
+Shirft : delete from start & end
+Slice () : return a piece of array
+Splice(): change original array(add, remove, replace */
---
+++ Day 15/script.js
@@ -0,0 +1,80 @@
+function sum(x, y) {
+  return x * y;
+}
+
+console.log(sum(4, 5));
+
+let value = sum(5, 6);
+console.log(value);
+
+const mul = (a, b) => {
+  console.log(a * b);
+};
+
+mul(4, 9);
+
+function countVowels(str) {
+  let count = 0;
+  for (const char of str) {
+    console.log(char);
+    if (char == "a" || char == "e" || char == "i" || char == "o" || char == "u")
+      count++;
+  }
+  console.log(`The the total count is ${count}`);
+}
+
+countVowels("alfie");
+
+const countVow = (str) => {
+  let count = 0;
+  for (const char of str) {
+    console.log(char);
+    if (char == "a" || char == "e" || char == "i" || char == "o" || char == "u")
+      count++;
+  }
+  console.log(`The the total count is ${count}`);
+};
+
+countVow("Saijeet");
+
+function abc() {
+  console.log("Hello");
+}
+
+let arr = [1, 2, 3, 4, 5];
+// arr.forEach(function printVal(val) {
+//   //
+//   console.log(val);
+// });
+arr.forEach((val) => {
+  console.log(val);
+});
+
+let cities = ["New York", "KTM", "San Francisco", "Chicago"];
+
+arr.forEach((val, idx, arr) => {
+  console.log(val, idx, arr);
+});
+
+const array = [1, 2, 5, 7];
+
+const square = (num) => {
+  console.log(num * num);
+};
+array.forEach((val) => {
+  console.log(val * val);
+});
+
+array.forEach(square);
+
+let newArr = array.map((num) => {
+  return num * num;
+});
+
+console.log(newArr);
+
+let filArr = array.filter((val) => {
+  return val % 2 === 0;
+});
+
+console.log(filArr);
