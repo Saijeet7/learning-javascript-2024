319605ac03fd7092a28134040fb66cfe63f3d05a
Saijeet Upadhyay <saijeet2019@gmail.com>
2024-02-21 17:00:42 +0545

    Complete : minor changes

--- API/script.js
+++ API/script.js
@@ -1,3 +1,60 @@
-const URL = "https://cat-fact.herokuapp.com/facts";
-const factPara = document.querySelector("#fact");
-const btn = document.querySelector(".btn");
+const baseURL =
+  "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/";
+
+const dropDowns = document.querySelectorAll(".dropdown select");
+const btn = document.querySelector("form button");
+const fromCurr = document.querySelector(".from select");
+const toCurr = document.querySelector(".to select");
+const msg = document.querySelector(".msg");
+
+let i = 0;
+for (let select of dropDowns) {
+  for (code in countryList) {
+    let newOption = document.createElement("option");
+    newOption.innerText = code;
+    newOption.value = code;
+    if (select.name === "from" && code === "USD") {
+      newOption.selected = "selected";
+    } else if (select.name === "to" && code === "NPR") {
+      newOption.selected = "selected";
+    }
+    select.append(newOption);
+  }
+  select.addEventListener("change", (e) => {
+    updateFlag(e.target);
+  });
+}
+
+const updateExchnageRate = async () => {
+  let amount = document.querySelector("form input");
+  let amtValue = amount.value;
+  if (amtValue === "" || amtValue < 1) {
+    amtValue = 1;
+    amount.value = 1;
+  }
+  const URL = `${baseURL}/${fromCurr.value.toLowerCase()}/${toCurr.value.toLowerCase()}.json`;
+  let response = await fetch(URL);
+  let data = await response.json();
+  let rate = data[toCurr.value.toLowerCase()];
+  let finalAmount = amtValue * rate;
+  msg.innerText = `${amtValue} ${fromCurr.value} = ${finalAmount.toFixed(2)} ${toCurr.value}`;
+  console.log(finalAmount);
+};
+
+const updateFlag = (element) => {
+  console.log(element);
+  const code = element.value;
+  const countryCode = countryList[code];
+  const newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
+  const img = element.parentElement.querySelector("img");
+  img.src = newSrc;
+};
+
+btn.addEventListener("click", async (e) => {
+  event.preventDefault();
+  updateExchnageRate();
+});
+
+window.addEventListener("load", () => {
+  updateExchnageRate();
+});
