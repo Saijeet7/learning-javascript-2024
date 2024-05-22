eee59f98bb7018a48bb6b88a366937556652fd54
Saijeet Upadhyay <saijeet2019@gmail.com>
2024-05-22 20:12:05 +0545

    Complete : modern operators and strings

--- Modern JS/script.js
+++ Modern JS/script.js
@@ -1,8 +1,6 @@
 "use strict";
 
 // Data needed for a later exercise
-const flights =
-  "_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30";
 
 // Data needed for first part of the section
 
@@ -696,3 +694,71 @@ GOOD LUCK 😀
 // console.log(maskCreditCard(234234));
 // console.log(maskCreditCard(2141293124));
 // console.log(maskCreditCard(123123123121233));
+
+///////////////////////////////////////
+// Coding Challenge #4
+
+/* 
+Write a program that receives a list of variable names written in underscore_case and convert them to camelCase.
+
+The input will come from a textarea inserted into the DOM (see code below), and conversion will happen when the button is pressed.
+
+THIS TEST DATA (pasted to textarea)
+underscore_case
+ first_name
+Some_Variable 
+  calculate_AGE
+delayed_departure
+
+SHOULD PRODUCE THIS OUTPUT (5 separate console.log outputs)
+underscoreCase      ✅
+firstName           ✅✅
+someVariable        ✅✅✅
+calculateAge        ✅✅✅✅
+delayedDeparture    ✅✅✅✅✅
+
+HINT 1: Remember which character defines a new line in the textarea 😉
+HINT 2: The solution only needs to work for a variable made out of 2 words, like a_b
+HINT 3: Start without worrying about the ✅. Tackle that only after you have the variable name conversion working 😉
+HINT 4: This challenge is difficult on purpose, so start watching the solution in case you're stuck. Then pause and continue!
+
+Afterwards, test with your own test data!
+
+GOOD LUCK 😀
+*/
+
+document.body.append(document.createElement("textarea"));
+document.body.append(document.createElement("button"));
+
+document.querySelector("button").addEventListener("click", () => {
+  let textInput = document.querySelector("textarea").value;
+  console.log(textInput);
+  const rows = textInput.split("\n");
+  let count = 0;
+  for (const row of rows) {
+    count++;
+    const [firstWord, secondWord] = row.toLowerCase().trim().split("_");
+    const output = `${firstWord}${secondWord.replace(
+      secondWord[0],
+      secondWord[0].toUpperCase()
+    )}`;
+    console.log(output.padEnd(20, " ") + "✅".repeat(count));
+  }
+});
+
+const flights =
+  "_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30";
+
+const flightSeperate = flights.split("+");
+console.log(flightSeperate);
+
+const getCode = str => str.slice(0, 3).toUpperCase();
+
+for (const flight of flightSeperate) {
+  const [type, from, to, time] = flight.split(";");
+  const output = `${type.startsWith("_Delayed") ? "🔴" : " "}${type.replaceAll(
+    "_",
+    " "
+  )} from ${getCode(from)} to ${getCode(to)} (${time.replace(":", "h")})`.padStart(50);
+  console.log(output);
+}
