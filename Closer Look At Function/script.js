4a44d18bce70c101222bfedc324e875b1f62e48b
Saijeet Upadhyay <saijeet2019@gmail.com>
2024-05-25 21:29:18 +0545

    Complete : functions

--- Closer Look At Function/script.js
+++ Closer Look At Function/script.js
@@ -42,35 +42,33 @@
 // checkIn(flight, saijeet);
 // console.log(flight);
 // console.log(saijeet);
-/*
-const oneWord = function (str) {
-  return str.replace(/ /g, '').toLowerCase();
-};
 
-const upperFirstWord = function (str) {
-  const [first, ...others] = str.split(' ');
-  return [first.toUpperCase(), ...others].join(' ');
-};
+// const oneWord = function (str) {
+//   return str.replace(/ /g, '').toLowerCase();
+// };
 
-// Higher-oder function
-const transformer = function (str, fn) {
-  console.log(`Original string : ${str}`);
-  console.log(`Transformed string : ${fn(str)}`);
-  console.log(`Transformed by : ${fn.name}`);
-};
+// const upperFirstWord = function (str) {
+//   const [first, ...others] = str.split(' ');
+//   return [first.toUpperCase(), ...others].join(' ');
+// };
 
-transformer('JavaScript is the best!', upperFirstWord);
-transformer('JavaScript is the best!', oneWord);
+// // Higher-oder function
+// const transformer = function (str, fn) {
+//   console.log(`Original string : ${str}`);
+//   console.log(`Transformed string : ${fn(str)}`);
+//   console.log(`Transformed by : ${fn.name}`);
+// };
 
-// JS uses callbacks all the time
-const high5 = function () {
-  console.log('Hello');
-};
+// transformer('JavaScript is the best!', upperFirstWord);
+// transformer('JavaScript is the best!', oneWord);
 
-document.body.addEventListener('click', high5);
-['Jonas', 'Martha', 'Adam'].forEach(high5);
+// // JS uses callbacks all the time
+// const high5 = function () {
+//   console.log('Hello');
+// };
 
-*/
+// document.body.addEventListener('click', high5);
+// ['Jonas', 'Martha', 'Adam'].forEach(high5);
 
 // const greet = function (greeting) {
 //   return function (name) {
@@ -96,52 +94,200 @@ document.body.addEventListener('click', high5);
 // arrowHey('Axl');
 
 // The call and apply method
-const lufthansa = {
-  airline: 'Lufthansa',
-  iataCode: 'LH',
-  bookings: [],
-  book(flightNum, name) {
-    console.log(
-      `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
-    );
-    this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
-  },
+// const lufthansa = {
+//   airline: 'Lufthansa',
+//   iataCode: 'LH',
+//   bookings: [],
+//   book(flightNum, name) {
+//     console.log(
+//       `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
+//     );
+//     this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
+//   },
+// };
+
+// lufthansa.book(238, 'Saijeet Upadhyay');
+// lufthansa.book(383, 'Saikriti Upadhyay');
+// console.log(lufthansa);
+
+// const eurowings = {
+//   airline: 'Eurowings',
+//   iataCode: 'EW',
+//   bookings: [],
+// };
+
+// const book = lufthansa.book;
+
+// // call method
+// book.call(eurowings, 23, 'Sarh  Wings');
+// console.log(eurowings);
+
+// book.call(lufthansa, 243, 'Sheldon Cooper');
+// console.log(lufthansa);
+
+// const swiss = {
+//   airline: 'Swiss Air Lines',
+//   iataCode: 'LX',
+//   bookings: [],
+// };
+
+// book.call(swiss, 482, 'Marry Cooper');
+// console.log(swiss);
+
+// // Apply Method
+// const flightData = [12, 'Geoge Cooper'];
+// book.apply(swiss, flightData);
+// console.log(swiss);
+
+// book.call(swiss, ...flightData);
+
+// const bookEW = book.bind(eurowings);
+// const bookLH = book.bind(lufthansa);
+// const bookLX = book.bind(swiss);
+
+// bookEW(23, 'Steven Williams');
+
+// const bookEW23 = book.bind(eurowings, 23);
+// bookEW23('Saijeet Upadhyay');
+// bookEW23('Stacy Cooper');
+
+// // With Event listeners
+// lufthansa.planes = 300;
+// lufthansa.buyPlane = function () {
+//   this.planes++;
+//   console.log(this.planes);
+// };
+
+// document
+//   .querySelector('.buy')
+//   .addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
+
+// // Partial Application
+// const addTax = (rate, value) => value + value * rate;
+// console.log(addTax(0.1, 200));
+
+// const addVAT = addTax.bind(null, 0.23);
+// console.log(addVAT(23));
+
+// const addTaxRate = rate => {
+//   return value => {
+//     return value + value * rate;
+//   };
+// };
+
+// const addVat2 = addTaxRate(0.1);
+// console.log(addVat2(200));
+
+///////////////////////////////////////
+// Coding Challenge #1
+
+/* 
+Let's build a simple poll app!
+
+A poll has a question, an array of options from which people can choose, and an array with the number of replies for each option. This data is stored in the starter object below.
+
+Here are your tasks:
+
+1. Create a method called 'registerNewAnswer' on the 'poll' object. The method does 2 things:
+  1.1. Display a prompt window for the user to input the number of the selected option. The prompt should look like this:
+        What is your favourite programming language?
+        0: JavaScript
+        1: Python
+        2: Rust
+        3: C++
+        (Write option number)
+  
+  1.2. Based on the input number, update the answers array. For example, if the option is 3, increase the value AT POSITION 3 of the array by 1. Make sure to check if the input is a number and if the number makes sense (e.g answer 52 wouldn't make sense, right?)
+2. Call this method whenever the user clicks the "Answer poll" button.
+3. Create a method 'displayResults' which displays the poll results. The method takes a string as an input (called 'type'), which can be either 'string' or 'array'. If type is 'array', simply display the results array as it is, using console.log(). This should be the default option. If type is 'string', display a string like "Poll results are 13, 2, 4, 1". 
+4. Run the 'displayResults' method at the end of each 'registerNewAnswer' method call.
+
+HINT: Use many of the tools you learned about in this and the last section 😉
+
+BONUS: Use the 'displayResults' method to display the 2 arrays in the test data. Use both the 'array' and the 'string' option. Do NOT put the arrays in the poll object! So what shoud the this keyword look like in this situation?
+
+BONUS TEST DATA 1: [5, 2, 3]
+BONUS TEST DATA 2: [1, 5, 3, 9, 6, 1]
+
+GOOD LUCK 😀
+*/
+
+// const poll = {
+//   question: 'What is your favourite programming language?',
+//   options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
+//   // This generates [0, 0, 0, 0]. More in the next section 😃
+//   answers: new Array(4).fill(0),
+//   registerNewAnswer() {
+//     // get answer
+//     const answer = Number(
+//       prompt(
+//         `${this.question}\n${this.options.join('\n')}\n(Write option number)`
+//       )
+//     );
+//     console.log(answer);
+//     // Register Answer
+//     typeof answer === 'number' &&
+//       answer <= this.answers.length &&
+//       this.answers[answer]++;
+//     console.log(this.answers);
+//     this.displayResult();
+//     this.displayResult('string');
+//   },
+//   displayResult(type = 'array') {
+//     if (type === 'array') {
+//       console.log(this.answers);
+//     } else if (type === 'string') {
+//       console.log(`Poll results are ${this.answers.join(', ')}`);
+//     }
+//   },
+// };
+
+// document
+//   .querySelector('.poll')
+//   .addEventListener('click', poll.registerNewAnswer.bind(poll));
+
+// poll.displayResult.call({ answers: [5, 2, 3] }, 'string');
+// poll.displayResult.call({ answers: [1, 5, 3, 9, 6, 1] }, 'string');
+
+//[5, 2, 3]
+// [1, 5, 3, 9, 6, 1]
+
+// IFFE
+(function () {
+  console.log('THis will never run again');
+})();
+
+(() => console.log('THis will never run again'))();
+
+// Closures
+const secureBooking = function () {
+  let passengerCount = 0;
+  return function () {
+    passengerCount++;
+    console.log(`${passengerCount} passengers`);
+  };
 };
 
-lufthansa.book(238, 'Saijeet Upadhyay');
-lufthansa.book(383, 'Saikriti Upadhyay');
-console.log(lufthansa);
+const booker = secureBooking();
+booker();
+booker();
+booker();
+console.dir(booker);
 
-const eurowings = {
-  airline: 'Eurowings',
-  iataCode: 'EW',
-  bookings: [],
+let f;
+const g = function () {
+  const a = 23;
+  f = function () {
+    console.log(a * 2);
+  };
 };
 
-const book = lufthansa.book;
+g();
+f();
 
-// does not work
-//book(23, 'Sarah');
 
-// call method
-book.call(eurowings, 23, 'Sarh  Wings');
-console.log(eurowings);
+(function(){
+  const header = document.querySelector('h1');
+  header.style.color = 'red';
+})
 
-book.call(lufthansa, 243, 'Sheldon Cooper');
-console.log(lufthansa);
-
-const swiss = {
-  airline: 'Swiss Air Lines',
-  iataCode: 'LX',
-  bookings: [],
-};
-
-book.call(swiss, 482, 'Marry Cooper');
-console.log(swiss);
-
-// Apply Method
-const flightData = [12, 'Geoge Cooper'];
-book.apply(swiss, flightData);
-console.log(swiss);
-
-book.call(swiss, ...flightData);
